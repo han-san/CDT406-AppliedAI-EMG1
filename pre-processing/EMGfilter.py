1a35d04e61538335858b9df4e7ff72e33285b911
OskarLindMDU <oskarlind.gaming@gmail.com>
2025-04-28 15:58:46 +0200

    Update EMGfilter.py

--- pre-processing/EMGfilter.py
+++ pre-processing/EMGfilter.py
@@ -4,6 +4,7 @@ import scipy as sp
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from scipy.signal import freqz
 
 # Load the CSV file without headers
 data = pd.read_csv(r'C:\Users\old22001\Downloads\WyoFlex_Dataset\WyoFlex_Dataset\VOLTAGE DATA\P1C1S1M1F1O1', header=None)
@@ -22,15 +23,64 @@ fs = 1000  # Sampling frequency in Hz
 # Design a low-pass Butterworth filter
 b, a = sp.signal.butter(order, cutoff / (fs / 2), btype='low', analog=False)
 
+print(b)
+print(a)
 
+# Create a window from the first 200 samples
+window = data_array[8000:11000]  # Adjust the indices as needed
 
-filtered_signal = sp.signal.lfilter(b, a, data_array)
+#calculate the mean and standard deviation
+mean = np.mean(window)
+std = np.std(window)
 
-plt.plot(data_array, label='Original Signal')
-plt.plot(filtered_signal, label='Filtered Signal')
+#mean subtraction
+window = window - mean
+
+#plot after mean subtraction
+plt.figure()
+plt.plot(window, label='Windowed Data (First 200 Samples)')
 plt.ylabel('Amplitude')
 plt.xlabel('Sample Number')
+plt.title('Windowed Data (Mean Subtracted)')
+plt.legend()
 plt.show()
 
-# Print the loaded data
-# print(data_array)
+
+
+#absolute value
+window = np.abs(window)
+
+#plot after absolute value
+plt.figure()
+plt.plot(window, label='Windowed Data (First 200 Samples)')
+plt.ylabel('Amplitude')
+plt.xlabel('Sample Number')
+plt.title('Windowed Data (Absolute Value)')
+plt.legend()
+plt.show()
+
+#normalize the data
+window_normalized = (window - mean) / std
+
+# Plot the normalized windowed data
+plt.figure()
+plt.plot(window_normalized, label='Windowed Data (First 200 Samples)')
+plt.ylabel('Amplitude')
+plt.xlabel('Sample Number')
+plt.title('Windowed Data (Normalized)')
+plt.legend()
+plt.show()
+
+# Apply the filter to the windowed data
+filtered_window = sp.signal.filtfilt(b, a, window)
+
+# plot the filtered windowed data
+plt.figure()
+plt.plot(filtered_window, label='Windowed Data (First 200 Samples)')
+plt.ylabel('Amplitude')
+plt.xlabel('Sample Number')
+plt.title('Windowed Data (Filtered)')
+plt.legend()
+plt.show()
+
+
