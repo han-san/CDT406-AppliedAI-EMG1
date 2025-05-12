a233c21ced2987d98935f81b5fe0267647aaaab1
OskarLindMDU <oskarlind.gaming@gmail.com>
2025-05-12 15:17:51 +0200

    More filters, choose normalization, switch to sosfiltfilt
    Filters for different frequency ranges now available to choose from
    
    choose between normalization type

--- src/preprocessing/FilterFunction.py
+++ src/preprocessing/FilterFunction.py
@@ -1,6 +1,6 @@
 import numpy as np
 from enum import Enum
-from scipy.signal import lfilter
+from scipy.signal import sosfiltfilt
 # from pathlib import Path
 
 
@@ -11,17 +11,64 @@ class filter (Enum):
     NO_FILTER = 0
     FILTER = 1
 
-def filter_function(data_array, filter):
+def filter_function(data_array, filter, filter_type=None, normalization_type=None):
     """
     This function applies mean subtraction, absolute value, normalization
     and a low-pass Butterworth filter to the input data array.
     Parameters: data_array (numpy.ndarray): The input data array to be processed.
                 filter (int): If 1, apply the filter; if 0, do not apply the filter.
+                filter_type (str): The type of filter to use ('cheby1_20to125', 'cheby1_125to250', 'butter_20to500').
+                normalization_type (str): The type of normalization to use ('min-max', 'z-score').
     """
-    # Numerators and denominators for a band-pass Butterworth filter
-    # 4th order band-pass Butterworth filter fs = 5000 Hz fc1 = 20 Hz fc2 = 600 Hz designed in Matlab
-    b1 = np.array([0.0042, 0., -0.0168, 0., 0.0252, 0., -0.0168, 0., 0.0042])
-    a1 = np.array([1.0000, -6.3816, 17.8992, -28.8773, 29.3561, -19.2729, 7.9812, -1.9056, 0.2009])
+    #cheby1 8th order pass ripple 0.1dB 20-250Hz
+    cheby1sos20to250 = np.array([
+        [7.74510118e-09, 1.54902024e-08, 7.74510118e-09, 1.00000000e+00, -1.83072505e+00, 8.74096486e-01],
+        [1.00000000e+00, 2.00000000e+00, 1.00000000e+00, 1.00000000e+00, -1.99680513e+00, 9.97401158e-01],
+        [1.00000000e+00, 2.00000000e+00, 1.00000000e+00, 1.00000000e+00, -1.98989275e+00, 9.90660250e-01],
+        [1.00000000e+00, 2.00000000e+00, 1.00000000e+00, 1.00000000e+00, -1.97582182e+00, 9.77159581e-01],
+        [1.00000000e+00, -2.00000000e+00, 1.00000000e+00, 1.00000000e+00, -1.86501440e+00, 9.66745068e-01],
+        [1.00000000e+00, -2.00000000e+00, 1.00000000e+00, 1.00000000e+00, -1.93806784e+00, 9.41707601e-01],
+        [1.00000000e+00, -2.00000000e+00, 1.00000000e+00, 1.00000000e+00, -1.83248144e+00, 9.09366516e-01],
+        [1.00000000e+00, -2.00000000e+00, 1.00000000e+00, 1.00000000e+00, -1.86478941e+00, 8.80606308e-01]
+    ])
+    #cheby1 8th order pass ripple 0.1dB 20-125Hz
+    cheby1sos20to125 = np.array([
+        [1.64903657e-11, 3.29807315e-11, 1.64903657e-11, 1.00000000e+00, -1.93361479e+00, 9.46290611e-01],
+        [1.00000000e+00, 2.00000000e+00, 1.00000000e+00, 1.00000000e+00, -1.94330916e+00, 9.49367982e-01],
+        [1.00000000e+00, 2.00000000e+00, 1.00000000e+00, 1.00000000e+00, -1.94019212e+00, 9.60501057e-01],
+        [1.00000000e+00, 2.00000000e+00, 1.00000000e+00, 1.00000000e+00, -1.96502534e+00, 9.67496863e-01],
+        [1.00000000e+00, -2.00000000e+00, 1.00000000e+00, 1.00000000e+00, -1.98219393e+00, 9.83381300e-01],
+        [1.00000000e+00, -2.00000000e+00, 1.00000000e+00, 1.00000000e+00, -1.95987802e+00, 9.85511447e-01],
+        [1.00000000e+00, -2.00000000e+00, 1.00000000e+00, 1.00000000e+00, -1.99158800e+00, 9.92336515e-01],
+        [1.00000000e+00, -2.00000000e+00, 1.00000000e+00, 1.00000000e+00, -1.99716523e+00, 9.97766712e-01]
+    ])
+    #cheby1 8th order pass ripple 0.1dB 125-250Hz
+    cheby1sos125to250 = np.array([
+        [6.52154309e-11, 1.30430862e-10, 6.52154309e-11, 1.00000000e+00, -1.89173522e+00, 9.47185948e-01],
+        [1.00000000e+00, 2.00000000e+00, 1.00000000e+00, 1.00000000e+00, -1.96937367e+00, 9.93390749e-01],
+        [1.00000000e+00, 2.00000000e+00, 1.00000000e+00, 1.00000000e+00, -1.88738953e+00, 9.86710779e-01],
+        [1.00000000e+00, 2.00000000e+00, 1.00000000e+00, 1.00000000e+00, -1.95348905e+00, 9.79972356e-01],
+        [1.00000000e+00, -2.00000000e+00, 1.00000000e+00, 1.00000000e+00, -1.93424875e+00, 9.66317181e-01],
+        [1.00000000e+00, -2.00000000e+00, 1.00000000e+00, 1.00000000e+00, -1.87521450e+00, 9.63811101e-01],
+        [1.00000000e+00, -2.00000000e+00, 1.00000000e+00, 1.00000000e+00, -1.91250084e+00, 9.54094194e-01],
+        [1.00000000e+00, -2.00000000e+00, 1.00000000e+00, 1.00000000e+00, -1.87755312e+00, 9.49888670e-01]
+    ])
+    #butterworth 8th order pass ripple 0.1dB 20-500Hz
+    buttersos20to500 = np.array([
+        [0.00419699, 0.00839399, 0.00419699, 1., -1.10421484, 0.32796109],
+        [1., 2., 1., 1., -1.34463672, 0.65505184],
+        [1., -2., 1., 1., -1.95141778, 0.95212526],
+        [1., -2., 1., 1., -1.98137739, 0.98201597]
+    ])
+
+    if filter_type == 'cheby1_20to125':
+        sos = cheby1sos20to125
+    elif filter_type == 'cheby1_125to250':
+        sos = cheby1sos20to250
+    elif filter_type == 'butter_20to500':
+        sos = buttersos20to500
+    else:
+        raise ValueError("Invalid filter_type. Choose from 'cheby1_20to125', 'cheby1_125to250', or 'butter_20to500'.")
 
     # calculate the mean
     mean = float(np.mean(data_array))
@@ -30,27 +77,35 @@ def filter_function(data_array, filter):
     data_array_centered = data_array - mean
 
     # Apply the filter to the windowed data
-    # filtfilt means 8th order filter but zero phase distortion
+
     if filter == 1:
         # Apply the filter to the windowed data
-        filtered_data_array = lfilter(b1, a1, data_array_centered)
-        # absolute value
+        filtered_data_array = sosfiltfilt(sos, data_array_centered)
+
         filtered_data_array_abs = np.abs(filtered_data_array)
 
-        # normalize the data
-        data_array_filtered_output = (filtered_data_array_abs - np.min(filtered_data_array_abs)) / (
-                    np.max(filtered_data_array_abs) - np.min(filtered_data_array_abs))
 
-        # return filtered and normalized data
+        if normalization_type == 'z-score':
+            # z-score normalization
+            data_array_filtered_output = (filtered_data_array_abs - np.mean(filtered_data_array_abs)) / np.std(filtered_data_array_abs)
+        else:
+            # min-max normalization
+            data_array_filtered_output = (filtered_data_array_abs - np.min(filtered_data_array_abs)) / (np.max(filtered_data_array_abs) - np.min(filtered_data_array_abs))
+
+
         return data_array_filtered_output
     else:
         # absolute value
         data_array_centered_abs = np.abs(data_array_centered)
 
-        # normalize the data
-        data_array_output = (data_array_centered_abs - np.min(data_array_centered_abs)) / (
-                    np.max(data_array_centered_abs) - np.min(data_array_centered_abs))
-
+        if normalization_type == 'z-score':
+            # z-score normalization
+            data_array_output = (data_array_centered_abs - np.mean(data_array_centered_abs)) / np.std(data_array_centered_abs)
+        elif normalization_type == 'min-max':
+            # min-max normalization
+            data_array_output = (data_array_centered_abs - np.min(data_array_centered_abs)) / (np.max(data_array_centered_abs) - np.min(data_array_centered_abs))
+        else:
+            raise ValueError("Invalid normalization_type. Choose from 'z-score' or 'min-max'.")
         # return normalized data
         return data_array_output
 
