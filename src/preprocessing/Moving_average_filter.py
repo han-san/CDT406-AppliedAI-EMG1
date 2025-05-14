8aa426a74781528cd5788ff68ab49349e1f2910e
JoenMDU <jjt21003@student.mdu.se>
2025-05-14 13:39:05 +0200

    Made 2 moving average functions
    Moving average file now has 2 functions.
    One with exponential moving average and one with simple moving average with padding.

--- src/preprocessing/Moving_average_filter.py
+++ src/preprocessing/Moving_average_filter.py
@@ -6,16 +6,19 @@ from enum import Enum
 
 
 def calculate_moving_average(x):
-    #N=50
-    #cumsum = numpy.cumsum(numpy.insert(x, 0, 0))
-    #return (cumsum[N:] - cumsum[:-N]) / float(N)
     alpha = 0.1
     ema = np.zeros_like(x)
     x = np.array(x)
     ema[0] = x[0]
-    # Initialize with the first value
+    #Initialize with the first value
     for i in range(1, len(x)):
         ema[i] = alpha * x[i] + (1 - alpha) * ema[i - 1]
     return ema
-    # Call the function and print the result
 
+def simple_moving_average(x):
+    N = 50  # Window size
+    # Pad the input to avoid losing samples
+    padded_x = np.pad(x, (N, 0), mode='edge')
+    cumsum = np.cumsum(padded_x)
+    moving_avg = (cumsum[N:] - cumsum[:-N]) / float(N)
+    return moving_avg
