4ced4b50cac52fb4771344b75a82c2447f8d9bb0
Johan Sandred <johan.sandred@protonmail.com>
2025-05-15 15:39:01 +0200

    Add a script for checking the lengths of label sequences in measurement files

---
+++ src/check_shortest_label_sequence.py
@@ -0,0 +1,29 @@
+import sys
+import itertools
+from pathlib import Path
+
+import pandas as pd
+
+if len(sys.argv) != 2:
+    print("Usage: check_shortest_label_sequence.py [file|dir].")
+    sys.exit(1)
+
+path = Path(sys.argv[1])
+if not path.is_dir() and not path.is_file():
+    print("Argument is not a file or directory.")
+    sys.exit(1)
+
+def check_file(filepath: Path) -> None:
+    print(f"File: {filepath}")
+    voltages = pd.read_csv(filepath, header=0)
+    sequence_lengths = [len(list(g)) / 5000 for k, g in itertools.groupby(voltages.label)]
+    print("Sequence lengths in seconds:")
+    print(sequence_lengths)
+    print(f"Smallest sequence length: {min(sequence_lengths)}s")
+
+if path.is_file():
+    check_file(path)
+else:
+    for filepath in path.glob("**/*.csv"):
+        check_file(filepath)
+
