84a722dc02c167a7cd5dd7b66d1a339852d809a7
SuperJaremy <legoitcomp@gmail.com>
2025-05-19 16:29:58 +0200

    Improve logging

--- device/inference_res.py
+++ device/inference_res.py
@@ -1,10 +1,25 @@
 import struct
+from enum import Enum
+import array
+
+class State(Enum):
+	REST = 0
+	GRIP = 1
+	HOLD = 2
+	RELEASE = 3
+
+	def __str__(self):
+		return self.name
+
 
 in_pipe = '/home/EMG1/setup/inference_pipe'
 
 number_of_states = 4
+buffer_size = 1500
+start = 0
+step = 1 / 15_000
 
-struct_fmt = f'<{number_of_states}f'
+struct_fmt = f'<{buffer_size * 2}B{number_of_states}f'
 
 def max4(rest, grip, hold, release):
 	m = max(rest[1], grip[1], hold[1], release[1])
@@ -18,14 +33,25 @@ def max4(rest, grip, hold, release):
 		return release[0]
 
 f = open(in_pipe, "rb")
+log = open("states.log", "w")
 try:
 	while True:
 		received = f.read(struct.calcsize(struct_fmt))
 		if len(received) > 0:
-			rest, grip, hold, release = struct.unpack(struct_fmt, received)
-			print("\033[2J\r"+max4(("REST", rest), ("GRIP", grip), ("HOLD", hold), ("RELEASE", release)))
+			data = struct.unpack(struct_fmt, received)
+			samples = bytearray(data[0:-number_of_states])
+			rest, grip, hold, release = data[-number_of_states:]
+			res = max4((State.REST, rest), (State.GRIP, grip), (State.HOLD, hold), (State.RELEASE, release))
+			print("\033[2J\r"+str(res))
+			ar = array.array('H')
+			ar.frombytes(samples)
+			for i in ar:
+				log.write(f'{start}, {i}, {res.value}\n')
+				start += step
 		else:
 			break
 except KeyboardInterrupt:
 	f.close()
+	log.close()
 f.close()
+log.close()
--- src/run_on_device.py
+++ src/run_on_device.py
@@ -5,6 +5,8 @@ import array
 import time
 
 from data import Window, Input
+from preprocessing.FilterFunction import FilterType, NormalizationType
+from preprocessing.Moving_average_filter import MovingAverageType
 import numpy as np
 
 import tflite_runtime.interpreter as tflite
@@ -17,13 +19,26 @@ power = (1 << 12) - 1
 def value_to_voltage(val):
     return val * v_ref / power
 
+times = []
+inference_times = []
+
 def exit_gracefully():
-    print(sum(times)/len(times))
+    log = open('/home/EMG1/setup/model.log', 'w')
+    log.write('Waiting\n')
+    log.write(str(times))
+    log.write('\nInference\n')
+    log.write(str(inference_times))
+    log.close()
+    print(f"Average: {sum(times)/len(times)}, {sum(inference_times)/len(inference_times)}")
     adc_in.close()
     inference_out.close()
     exit(0)
 
 model_path = Path(sys.argv[1])
+
+filter_t = FilterType.RANGE_20_TO_500_BUTTER
+normalization_t = NormalizationType.Z_SCORE
+moving_t = MovingAverageType.EMA
 
 model = TFLiteModel(tflite.Interpreter, model_path)
 print('TFLite model is loaded')
@@ -31,37 +46,47 @@ print('TFLite model is loaded')
 buffer_size = 1500
 number_of_states = 4
 
-struct_fmt = f'<{buffer_size * 2}B'
+in_struct_fmt = f'<{buffer_size * 2}B'
+out_struct_fmt = f'<{buffer_size * 2}B{number_of_states}f'
 in_pipe = '/home/EMG1/setup/adc_pipe'
 out_pipe = '/home/EMG1/setup/inference_pipe'
 
 adc_in = open(in_pipe, "rb")
 inference_out = open(out_pipe, "wb")
 print('Starting')
-times = []
 
 try:
     while True:
         now = time.perf_counter()
-        received = adc_in.read(struct.calcsize(struct_fmt))
+        received = adc_in.read(struct.calcsize(in_struct_fmt))
         if len(received) > 0:
+            start = time.perf_counter()
             data = array.array('H')
             data.frombytes(received)
             del data[2::3]
             voltages = list(map(value_to_voltage, data))
-            window = Window(np.array(voltages, dtype=np.float32))
+            window = Window(np.array(voltages, dtype=np.float32), filter_t, normalization_t, moving_t)
             res = model.infer(Input(window))
-            now = time.perf_counter() - now
-            packed = struct.pack(f'<{number_of_states}f', res[0], res[1], res[2], res[3])
+            end = time.perf_counter()
+            now = end - now
+            start = end - start
+            packed = struct.pack(out_struct_fmt, *received, *res)
             try:
                 inference_out.write(packed)
                 inference_out.flush()
             except IOError:
-                print(sum(times)/len(times))
+                log = open('/home/EMG1/setup/model.log', 'w')
+                log.write('Waiting\n')
+                log.write(str(times))
+                log.write('\nInference\n')
+                log.write(str(inference_times))
+                log.close()
+                print(f"Average: {sum(times)/len(times)}, {sum(inference_times)/len(inference_times)}")
                 adc_in.close()
                 exit(0)
 
             times.append(now)
+            inference_times.append(start)
         else:
             break
 except KeyboardInterrupt:
